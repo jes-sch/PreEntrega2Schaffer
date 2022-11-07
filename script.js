600e126ff3a3738dd5033c17acd98b45660d5c1e
Jes Sch <jesica_schaffer@hotmail.com>
2022-11-07 15:38:15 -0300

    elimino comentarios innecesarios, para ordenar el código

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ if (ingresar==true){
     alert ("Ahora te voy a pedir unos datos");
     let productoElegido = parseInt(prompt("Elegí la opción que corresponda al producto que quieras comprar: \n1 - Remera \n2 - Pantalón \n3 - Hoodie"));
 
-/*     let inflacionMensualEstimada = 6.2; */
+/*     let inflacionMensualEstimada = 6.2; --> este dato también me lo guardo para más adelante*/
     
 if (productoElegido == 1) {
     alert("Pagando de contado te sale $ " + productosEnVenta[0].precio + " Pagando en cuotas te sale $ " + productosEnVenta[0].cuotas);
@@ -63,47 +63,9 @@ if (productoElegido == 1) {
         alert ("Elegiste una opción INEXISTENTE");
     }
 
-
 }else{
     alert ("Tu usuario quedó inhabilitado. Comunicate con el administrador.");
 }
-
-/* if (productoElegido == 1) {
-    alert("Pagando de contado te sale $ " + productosEnVenta.precio + " Pagando en cuotas te sale $ " + productosEnVenta.cuotas);
-} else if (productoElegido == 2) {
-    alert("Pagando de contado te sale $ " + productosEnVenta[precio] + " Pagando en cuotas te sale $ " + productosEnVenta[cuotas]);
-} else if (productoElegido == 3) {
-    alert("Pagando de contado te sale $ " + productosEnVenta[precio] + " Pagando en cuotas te sale $ " + productosEnVenta[cuotas]);
-} else {
-    alert ("Elegiste una opción INEXISTENTE");
-} */
-
-/* if (login()){
-    alert ("Te voy a pedir unos datos para determinar si te conviene comprar al contado o en cuotas,  y ganarle a la inflación (guiño guiño)");
-    let inflacionMensualEstimada = 6.2;
-    let precioEfectivo = parseInt(prompt("Ingresá el PRECIO TOTAL al CONTADO/EFECTIVO: "));
-    let precioEnCuotas = parseInt(prompt("Ingresá el PRECIO TOTAL en CUOTAS: "));
-    let cuotas = parseInt(prompt("Elegí la opción que corresponda a la cantidad de cuotas: \n1 - 3 cuotas \n2 - 6 cuotas \n3 - 9 cuotas \n4 - 12 cuotas"));
-    
-if (cuotas == 1) {
-    alert('Pagando en 3 cuotas, el valor de cada una va a ser de $ ' + (precioEnCuotas /3) + (inflacionMensualEstimada*3));
-} else if (cuotas == 2) {
-    alert('Pagando en 6 cuotas, el valor de cada una va a ser de $ ' + (precioEnCuotas /6) + (inflacionMensualEstimada*6));
-} else if (cuotas == 3) {
-    alert('Pagando en 9 cuotas, el valor de cada una va a ser de $ ' + (precioEnCuotas /9) + (inflacionMensualEstimada*9));
-} else if (cuotas == 4) {
-    alert('Pagando en 12 cuotas, el valor de cada una va a ser de $ ' + (precioEnCuotas /12) + (inflacionMensualEstimada*12));
-} else {
-    alert ("Elegiste una opción INEXISTENTE");
-}
-
-
-}else{
-    alert ("Tu usuario quedó inhabilitado. Comunicate con el administrador.");
-}
-
-alert("FIN DE LA ENTREGA HASTA AHORA. Programa en desarrollo ;)"); */
-
 
 /*     alert('Pagando en 3 cuotas, el valor de cada una va a ser de $ ' + (precioEnCuotas /3) + (inflacionMensualEstimada*3)); */
 
